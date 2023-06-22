2193908ed5c0475c409f941f604746a81228d3a4
wuchang1123 <wuchang1123@gmail.com>
2023-06-22 16:24:25 +0800

    url error

--- init/linode_wireguard.sh
+++ init/linode_wireguard.sh
@@ -16,19 +16,19 @@ WIREGUARD_PEER_PUBKEY=
 apt update
 apt-get install curl net-tools vim nano less apt-show-versions -y
 
-curl -o prep_system.sh $URL_PREFIX/raw/main/scripts/prep_system.sh
+curl -o prep_system.sh $URL_PREFIX/scripts/prep_system.sh
 chmod +x ./prep_system.sh
 ./prep_system.sh $LINODE_LISHUSERNAME
 
-curl -o configure_user.sh $URL_PREFIX/raw/main/scripts/configure_user.sh
+curl -o configure_user.sh $URL_PREFIX/scripts/configure_user.sh
 chmod +x ./configure_user.sh
 ./configure_user.sh $USER_NAME $USER_PASSWORD $SSH_PORT $USER_SSHKEY $SSH_ALLOW_USERS
 
-curl -o install_tailscale.sh $URL_PREFIX/raw/main/scripts/install_tailscale.sh
+curl -o install_tailscale.sh $URL_PREFIX/scripts/install_tailscale.sh
 chmod +x ./install_tailscale.sh
 ./install_tailscale.sh $TAILSCALE_AUTHKEY $LINODE_LISHUSERNAME
 
-curl -o install_wireguard.sh $URL_PREFIX/raw/main/scripts/install_wireguard.sh
+curl -o install_wireguard.sh $URL_PREFIX/scripts/install_wireguard.sh
 chmod +x ./install_wireguard.sh
 ./install_wireguard.sh wg0 $WIREGUARD_PRIKEY $WIREGUARD_PUBKEY $WIREGUARD_PEER_PUBKEY $WIREGUARD_PORT "10.10.0"
 ./install_wireguard.sh wg1 $WIREGUARD_PRIKEY $WIREGUARD_PUBKEY $WIREGUARD_PEER_PUBKEY $(($WIREGUARD_PORT+1000)) "10.10.1"
